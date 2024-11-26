9d5de5164e8070dc6d621d33a3b931452c80e48e
easyhyunn <102637680+easyhyunn@users.noreply.github.com>
2024-11-26 11:38:21 +0900

    키보드 테스트

--- new/main/js/main.js
+++ new/main/js/main.js
@@ -28,26 +28,21 @@ const keyNumCheck = () => {
 
 function virtualKeyboard(wraper) {
     var $keyboardWrapper = $(wraper),
-    $key = $keyboardWrapper.find("button"),
-    $key_delete = $(".delete-btn"),
-    $outputField = $(".search-text"),
+    $key = $keyboardWrapper.find('button'),
+    $key_delete = $('.delete-btn'),
+    $outputField = $('.search-text'),
     $currentValue = $outputField.val(),
-    actionKeys = $(".delete-btn");
+    actionKeys = $('.delete-btn');
   
   // handle keystrokes
   function _keystroke(keyCase) {
-    $key.not(actionKeys).on("click", function (e) {
+    $key.not(actionKeys).on('click', function (e) {
       e.preventDefault();
   
-      // handle case
-      if (keyCase == "upper") {
-        var keyValue = $(this).val().toUpperCase();
-      } else {
-        var keyValue = $(this).val().toLowerCase();
-      }
+      var keyValue = $(this).val();
   
       // grab current value
-      var output = $(".search-text").val();
+      var output = $('.search-text').val();
       $outputField.val(output + keyValue);
       getCurrentVal();
       focusOutputField();
@@ -55,7 +50,7 @@ function virtualKeyboard(wraper) {
   } // keystroke
   
   // delete
-  $key_delete.on("click", function (e) {
+  $key_delete.on('click', function (e) {
     e.preventDefault();
     $outputField.val($currentValue.substr(0, $currentValue.length - 1));
     getCurrentVal();
@@ -72,7 +67,8 @@ function virtualKeyboard(wraper) {
     $outputField.focus();
   }
   
-  _keystroke("lower"); // init keystrokes
+  _keystroke('lower'); // init keystrokes
+  
 }
 
 function onlyAlphaNum(input) {
@@ -83,7 +79,35 @@ function onlyAlphaNum(input) {
 }
 
 $(function() {
-    banner();
-    virtualKeyboard('.keyboard-numeric');
-    virtualKeyboard('.keyboard-eng');
+
+  const searchInput = $('.search-text');
+  const searchWrap = $('.key-wrap');
+
+
+  $('.tab_btn').on('click', function() {
+    searchInput.val('');
+    $('.key-tab').removeClass('active');
+    if ($(this).parents('.key-tab').hasClass('numeric-key')){
+      $('.numeric-key').addClass('active');
+    } else if ($(this).parents('.key-tab').hasClass('eng-key')){
+      $('.eng-key').addClass('active');
+    }
+  });
+
+  searchInput.on('click', function(e) {
+    if (searchWrap.hasClass('on') == false){
+      searchWrap.addClass('on');
+    } else {
+      e.preventDefault();
+    }
+  });
+
+  $('.close-btn').on('click', function() {
+    searchWrap.removeClass('on');
+  });
+
+  banner();
+  virtualKeyboard('.keyboard-numeric');
+  virtualKeyboard('.keyboard-eng');
+    
 });
